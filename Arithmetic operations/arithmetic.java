da77c1cc4fcaf87ae01032618569f3c8897593c2
HariKrishna2481 <156352988+HariKrishna2481@users.noreply.github.com>
2024-01-11 09:37:58 -0800

    Create Arithmetic.java

---
+++ Arithmetic operations/arithmetic.java
@@ -0,0 +1,10 @@
+public class Arithmetic{
+  public static void main(String[] args){
+    int a = 3;
+    int b = 8;
+    System.out.println("Addition of a, b: " + (a+b) );
+    System.out.println("Subtraction of a, b: " + (a-b) );
+    System.out.println("Multiplication of a, b: " + (a*b) );
+    System.out.println("Division of a, b: " + (double) (a/b) );
+  }
+}
