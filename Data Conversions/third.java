6d0bd6b5326a750c8faf61447442a57f0429cde8
HariKrishna2481 <156352988+HariKrishna2481@users.noreply.github.com>
2024-01-24 07:40:54 -0800

    Create third.java

---
+++ Data Conversions/third.java
@@ -0,0 +1,8 @@
+public class DoubleToStringConversion {
+    public static void main(String[] args) {
+        // Convert Double to String
+        double doubleValue = 3.14159;
+        String stringValue = Double.toString(doubleValue);
+        System.out.println("Double to String: " + stringValue);
+    }
+}
